9b9462b33d7f22187ffc8f9eab41418d41f18075
“Enter <talktoparthi@gmail.com>
2020-08-04 20:22:41 +0530

    changed echo

--- ArithmeticSort.sh
+++ ArithmeticSort.sh
@@ -1,6 +1,6 @@
 #!bin/bash -x
 #taking inputs
-echo "sorting conflict"
+echo "sorting conflict 1 created by parthiban"
 read -p "Enter value of a:" a
 read -p "Enter value of b:" b
 read -p "Enter value of c:" c
